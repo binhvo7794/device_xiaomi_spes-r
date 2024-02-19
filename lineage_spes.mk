aa502ee959c97e3589e6a8ae0c9724deb7768c2a
Guidix <81136142+GuidixX@users.noreply.github.com>
2024-02-19 14:25:54 +0200

    spes: CrDroid Bringup!

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -9,19 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-USE_PIXEL_CHARGING := true
-PRODUCT_NAME := aosp_spes
+# CrDroid Stuff
+TARGET_DEVICE_MAINTAINER := GuidixX
+
+# Device Identifier
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="spes-user 11 RKQ1.211001.001 V13.0.9.0.RGCMIXM release-keys"
