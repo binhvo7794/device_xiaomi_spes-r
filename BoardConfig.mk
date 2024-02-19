533fa030d2387291554593f914087e7c906a8997
Guidix <81136142+GuidixX@users.noreply.github.com>
2024-02-19 17:34:22 +0200

    spes: Fix compatibility_matrix patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,7 @@ BOARD_HAVE_QCOM_FM := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += k7tn
