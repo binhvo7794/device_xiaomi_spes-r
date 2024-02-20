c4ef6e9006b91051064192f6c48d85fd1f7a6d7a
Guidix <81136142+GuidixX@users.noreply.github.com>
2024-02-20 21:32:30 +0200

    spes: Introduce Lineage Overlays

--- device.mk
+++ device.mk
@@ -418,6 +418,11 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+	$(LOCAL_PATH)/overlay-lineage \
+
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
