6e9534890865910bcc6facea2f74c217ddd82655
Guidix <guidixm@gmail.com>
2024-03-05 11:11:35 +0200

    spes: Now OTA is supported

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 echo "Syncing additional trees"
 rm -rf hardware/xiaomi/megvii
 rm -rf packages/apps/crDroidSettings && git clone https://github.com/GuidixX/android_packages_apps_crDroidSettings packages/apps/crDroidSettings
+rm -rf vendor/crDroidOTA && git clone https://github.com/GuidixX/android_vendor_crDroidOTA vendor/crDroidOTA
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-18.1
 git clone https://github.com/GuidixX/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
