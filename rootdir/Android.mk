858c1585339ec976246b7b986eea32e9449abab6
Guidix <guidixm@gmail.com>
2024-05-20 14:07:57 +0300

    spes: Add fstab zram to android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -67,6 +67,14 @@ LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.zram
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.zram
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom_ramdisk
 LOCAL_MODULE_STEM  := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
